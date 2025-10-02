f59f6fc53b08cfd355b4134b6a90bb083a4537c5
FiTTrA990 <fitravg5@gmail.com>
2025-10-02 14:38:48 +0700

    Update Person.py

--- Python/Person.py
+++ Python/Person.py
@@ -1,1 +1,33 @@
+# Person.py
+# Kelas dasar untuk semua orang di rumah sakit
+
+class Person:
+    def __init__(self, nama="", notelp="", alamat=""):
+        self.__nama = nama
+        self.__notelp = notelp
+        self.__alamat = alamat
+
+    # Setter
+    def set_nama(self, nama):
+        self.__nama = nama
+
+    def set_notelp(self, notelp):
+        self.__notelp = notelp
+
+    def set_alamat(self, alamat):
+        self.__alamat = alamat
+
+    # Getter
+    def get_nama(self):
+        return self.__nama
+
+    def get_notelp(self):
+        return self.__notelp
+
+    def get_alamat(self):
+        return self.__alamat
+
+    # Method untuk print info dasar
+    def print_info(self):
+        print(f"Nama: {self.__nama}, Telp: {self.__notelp}, Alamat: {self.__alamat}", end="")
 
