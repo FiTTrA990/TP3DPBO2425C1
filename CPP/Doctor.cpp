51b3aeeec62ac921e7b9b1f157711347a4e2fdf5
FiTTrA990 <fitravg5@gmail.com>
2025-10-02 14:07:07 +0700

    Update Doctor.cpp

--- CPP/Doctor.cpp
+++ CPP/Doctor.cpp
@@ -1,1 +1,40 @@
+#ifndef DOCTOR_CPP
+#define DOCTOR_CPP
+
+#include <iostream>
+#include "Person.cpp"
+using namespace std;
+
+class Doctor : public Person {
+private:
+    string idDokter;
+    string spesialis;
+    string jadwalPraktek;
+
+public:
+    Doctor() {}
+    Doctor(string nama, string notelp, string alamat, string id, string spesialis, string jadwal)
+        : Person(nama, notelp, alamat) {
+        this->idDokter = id;
+        this->spesialis = spesialis;
+        this->jadwalPraktek = jadwal;
+    }
+
+    void setIdDokter(string id) { idDokter = id; }
+    void setSpesialis(string spesialis) { this->spesialis = spesialis; }
+    void setJadwalPraktek(string jadwal) { jadwalPraktek = jadwal; }
+
+    string getIdDokter() const { return idDokter; }
+    string getSpesialis() const { return spesialis; }
+    string getJadwalPraktek() const { return jadwalPraktek; }
+
+    void printDoctor() const {
+        cout << "[Doctor] "; 
+        Person::printInfo();
+        cout << ", ID: " << idDokter << ", Spesialis: " << spesialis 
+             << ", Jadwal: " << jadwalPraktek << endl;
+    }
+};
+
+#endif
 
